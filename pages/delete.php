ae69f8a159188c3bb411c23536d2c222517209d2
Lyz03 <lizoe@netcourrier.com>
2022-02-23 12:22:08 +0100

    delete

--- pages/delete.php
+++ pages/delete.php
@@ -1,1 +1,8 @@
 <?php
+$stmt = $db->prepare("DELETE FROM hiking WHERE id = :id");
+$id = (int) $_GET['id'];
+$stmt->bindParam(':id', $id);
+
+if ($stmt->execute()) {
+    header('Location: ' . __DIR__ . '/../pages/read.php');
+}
